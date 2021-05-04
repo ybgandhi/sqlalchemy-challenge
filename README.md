0f50cd06cb823469ec6c5e1ab51d890dff6e70f5
ybgandhi <gandhibyash@gmail.com>
2021-05-04 19:40:22 -0400

    final updates

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# sqlalchemy-challenge
+Clime Analysis using SQLAlchemy
+
+This analysis requred two Parts
+    Part 1: Climate Analysis and Exploration
+    Part 2: CLimate App
+
+Step 1: Climate Analysis and Exploration
+Use Python and SQLAlchemy to do basic analysis and data exploration of the database provided.
+
--- app.py
+++ app.py
@@ -14,7 +14,7 @@ engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={'check
 # reflect database into new model
 Base = automap_base
 # reflect tables
-Base.prepare(enginge, reflect=True)
+Base.prepare(engine, reflect=True)
 
 # Save references of each table
 mesurement = Base.classes.measurement
@@ -31,10 +31,90 @@ def home():
     print("In & Out of Home section.")
     return (
         f"Welcome to the Hawaii Climate API!<br/>"
-        f"Please select one of the available routes:<br/>""
+        f"Please select one of the available routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/
-        f"/api/v1.0/
-        f"/api/v1.0/
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/2016-01-01/<br/>"
+        f"/api/v1.0/2016-01-01/2016-12-31/"
     )
+
+    # return the JSON representation of the dictionary that was created
+    @app.route('/api/v1.0/precipitation/')
+    def precipitation():
+        print("In Precipitation section.")
+
+        last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first().date
+        last_year = dt.datetime.strptime(last_date, '%Y-%m-%d') - dt.timedelta(days=365)
+
+        rain_results = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= last_year). \
+        order_by(mesurement.date).all()
+
+        p_dict = dict(rain_results)
+        print(f"Results for Precipitation - {p_dict}")
+        print(f"Out of Precipitation section.")
+        return jsonify(p_dict)
+    
+    # return JSON list of stations from the dataset
+    @app.route('/api/v1.0/stations/')
+    def stations():
+        print()
+
+        station_list = session.query(Station.station)\
+        .order_by(Station.station).all()
+        print()
+        print("Station List:")
+        print(row[0])
+        print("Out of Station section.")
+        return jsonify(station_list)
+
+    # return JSON list of Temperature Observations from the dataset
+    @app.route('/api/v1.0/tobs/')
+    def tobs():
+        print("In TOBS section")
+
+        last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first().date
+        last_year = dt.datetime.strptime(last_date, '%Y-%m-%d') - dt.timedelta(days=365)
+
+        temp_obs = session.query(Measurement.date, Measurement.tobs)\
+            .filter(Measurement.date >= last_year)\
+            .order_by(Measurement.date).all()
+
+        print()
+        print("Temperature Results for All Stations")
+        print(temp_obs)
+        print("Out of TOBS section")
+        return jsonify(temp_obs)
+
+    # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start date    
+    @app.route('/api/v1.0/<start_date>/')
+    def calc_temps_start(start_date):
+        print("In start date section.")
+        print(start_date)
+
+        select = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+        result_temp = session.query(*select).\
+            filter(Measurement.date >= start_date).all()
+        print()
+        print(f"Calculated temp for start date {start_Date}")
+        print(result_temp)
+        print("Out of start date section")
+        return jsonify(result_temp)
+
+    # Return a JSON list of the minimum temperature, the average temperature, and the max remperature for the end date
+    @app.route('/api/v1.0/<start_date>/<end_datet>')
+    def calc_temps_end(start_date, end_date):
+        print("In start & end date section")
+
+        select = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+        result_temp = session.query(*select).\
+            filter(Measurement.date >= start_date).filter(Measurement.tobs <= end_date).all()
+        print()
+        print(f"Calculated temp for start date {start_date} & end date {end_date}")
+        print(result_temp)
+        print("Out of start & end date section.")
+        return jsonify(result_temp)
+
+    if __name__ == "__main__":
+        app.run(debug=True)
